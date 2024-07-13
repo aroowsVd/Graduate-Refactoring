2cac9d5f1b97bfd81f5400adc82a8790c7d68f86
aroowsVd <noya716@naver.com>
2024-07-13 22:18:33 +0900

    main m_menu03

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -13,6 +13,34 @@ document.addEventListener("DOMContentLoaded", function () {
       nav.style.height = "90px";
     });
   });
+
+  // // mobile menu 아코디언
+  // let mainLists = document.querySelectorAll(".m_main_li");
+
+  // mainLists.forEach(function(mainList) {
+  //   mainList.addEventListener("click", function() {
+  //     let subList = mainList.nextElementSibling;
+
+  //     if(subList.classList.contains("m_sub_li")) {
+  //       if(subList.style.display === "block") {
+  //         subList.style.display = "none";
+  //       } else {
+  //         document.querySelectorAll(".m_sub_li").forEach(function(otherSubLi) {
+  //           otherSubLi.style.display = "none";
+  //         });
+
+  //         subList.style.display = "block"
+  //       }
+
+  //       mainList.classList.toggle("on");
+  //       mainLists.forEach(function(otherMainList) {
+  //         if (otherMainList !== mainList) {
+  //           otherMainList.classList.remove("on");
+  //         }
+  //       })
+  //     }
+  //   });
+  // });
 });
 
 // 초기 로드 시 로고 변경 함수 호출
@@ -50,4 +78,15 @@ menuWrap.addEventListener("click", function (event) {
   if (event.target === menuWrap) {
     menuWrap.classList.remove("show");
   }
+});
+
+
+// mobile menu 아코디언
+$(".m_main_li").click(function() {
+  $(this).next(".m_sub_li").stop().slideToggle(300);
+  $(this).toggleClass('on').siblings().removeClass('on');
+  $(this).next(".m_sub_li").siblings(".m_sub_li").slideUp(300); // 1개씩 펼치기
+
+  $(this).parent().siblings().children('.m_main_li').removeClass('on');
+  $(this).parent().siblings().children('.m_sub_li').stop().slideUp(300);  
 });
